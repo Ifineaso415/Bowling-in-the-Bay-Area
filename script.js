35813be6cfcf4c83d27e9dbcf79443b815e6a50a
Ifineaso415 <IsaacFineaso@gmail.com>
2025-08-12 18:37:08 -0700

    l

--- script.js
+++ script.js
@@ -1,63 +1,84 @@
 "use strict";
 
-const Pickle415 =
-  "https://api.airtable.com/v0/appyPO0xrhPPEy72s/Outdoor";
+const OUTDOOR_URL = "https://api.airtable.com/v0/appyPO0xrhPPEy72s/Outdoor";
+const INDOOR_URL = "https://api.airtable.com/v0/appyPO0xrhPPEy72s/Indoor";
 
-// function for our list view
-async function fetchAlleys() {
+// function for our list view (both types)
+async function fetchCourts() {
   let getResultElement = document.getElementById("container");
-
   const options = {
     method: "GET",
     headers: {
       Authorization: `Bearer patSMmsMZV3ld7iSm.adf9b201bf5b4fad908e372c585816eb2521b5e7086b7a3c9418caaa099ad817`
     },
   };
+  // Fetch both Outdoor and Indoor courts in parallel
+  const [outdoorData, indoorData] = await Promise.all([
+    fetch(OUTDOOR_URL, options).then((response) => response.json()),
+    fetch(INDOOR_URL, options).then((response) => response.json()),
+  ]);
+  // Combine both records
+  const allRecords = [...outdoorData.records, ...indoorData.records];
+  renderCourts(allRecords);
+}
 
-  await fetch(
-    `${Pickle415}`,
-    options
-  )
-    .then((response) => response.json())
+// Only Outdoor courts
+async function fetchOutdoorCourts() {
+  let getResultElement = document.getElementById("container");
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer patSMmsMZV3ld7iSm.adf9b201bf5b4fad908e372c585816eb2521b5e7086b7a3c9418caaa099ad817`
+    },
+  };
+  const outdoorData = await fetch(OUTDOOR_URL, options).then((response) => response.json());
+  renderCourts(outdoorData.records);
+}
 
-    .then((data) => {
-      console.log(data); // response is an object w/ .records array
+// Only Indoor courts
+async function fetchIndoorCourts() {
+  let getResultElement = document.getElementById("container");
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer patSMmsMZV3ld7iSm.adf9b201bf5b4fad908e372c585816eb2521b5e7086b7a3c9418caaa099ad817`
+    },
+  };
+  const indoorData = await fetch(INDOOR_URL, options).then((response) => response.json());
+  renderCourts(indoorData.records);
+}
 
-      getResultElement.innerHTML = ""; // clear alleys
-
-      let newHtml = "";
-
-      for (let i = 0; i < data.records.length; i++) {
-        let courtName = data.records[i].fields["Name"];
-        let courtAddress = data.records[i].fields["Address"];
-        let courtZip = data.records[i].fields["Zip"];
-        let courtMap = data.records[i].fields["Map"];
-        let courtNumberOfCourts = data.records[i].fields["Number of Courts"];
-        let courtAvailability = data.records[i].fields["Availability"];
-        let courtImage = data.records[i].fields["Images"];
-
-        newHtml += `
-        
-          <div class="col-md-4 alley-card">
-            <div class="card">
-              ${courtImage ? `<img src="${courtImage[0].url}" alt="Photo of ${courtName}">` : ``}
-              <div class="card-body">
-                <h5 class="card-title">
-                   ${courtName}
-                </h5>
-                <p>${courtAddress}</p>
-                <a class="mt-1 btn btn-primary mt-2" href="index.html?id=${
-                  data.records[i].id
-                }">View Details</a>
-              </div>
-            </div>
+// Helper to render courts
+function renderCourts(records) {
+  let getResultElement = document.getElementById("container");
+  getResultElement.innerHTML = "";
+  let newHtml = "";
+  for (let i = 0; i < records.length; i++) {
+    let courtName = records[i].fields["Name"];
+    let courtAddress = records[i].fields["Address"];
+    let courtZip = records[i].fields["Zip"];
+    let courtMap = records[i].fields["Map"];
+    let courtNumberOfCourts = records[i].fields["Number of Courts"];
+    let courtAvailability = records[i].fields["Availability"];
+    let courtImage = records[i].fields["Images"];
+    newHtml += `
+      <div class="col-md-4 court-card">
+        <div class="card">
+          ${courtImage ? `<img src="${courtImage[0].url}" alt="Photo of ${courtName}">` : ``}
+          <div class="card-body">
+            <h5 class="card-title">
+               ${courtName}
+            </h5>
+            <p>${courtAddress}</p>
+            <a class="mt-1 btn btn-primary mt-2" href="index.html?id=${
+              records[i].id
+            }">View Details</a>
           </div>
-    
-        `;
-      }
-
-      getResultElement.innerHTML = newHtml;
-    });
+        </div>
+      </div>
+    `;
+  }
+  getResultElement.innerHTML = newHtml;
 }
 
 function formatPhoneNumber(phoneNumberString) {
@@ -70,84 +91,78 @@ function formatPhoneNumber(phoneNumberString) {
   return null;
 }
 
-function fetchSingleAlley(alleyId) {
-  let alleyResultElement = document.getElementById("alley-container");
-  
-    const options = {
+function fetchSingleCourt(courtId) {
+  let courtResultElement = document.getElementById("court-container");
+  const options = {
     method: "GET",
     headers: {
       Authorization: `Bearer patuwPLWf5SBeDkyT.45118f887bbc6a79e8bb6c9d327b8c8105866bb0d6a0cdc2290006ddec22a74d`,
     },
   };
-
-    fetch(`${BOWLING_URL}/${alleyId}`,
-        options
-       )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data); // response is a single object
-
-      let alleyPic = data.fields["image"];
-      let alleyName = data.fields["name"];
-      let alleyDescription = data.fields["description"];
-      let alleyAddress = data.fields["address"];
-      let alleyHours = data.fields["hours"];
-      let alleyURL = data.fields["url"];
-      let alleyPhone = formatPhoneNumber(data.fields["phone"]);
-      let alleyLanes = data.fields["lanes"];
-      let alleyCost = data.fields["cost"];
-      let alleyLeagues = data.fields["league"];
-
-      let hoursHtml = "";
-      if ("hours" in data.fields) {
-        hoursHtml += "<ul>";
-        let hours = data.fields["hours"].split("\n\n");
-        for (let i = 0; i < hours.length; i++) {
-          hoursHtml += `<li>${hours[i]}</li>`;
-        }
-        hoursHtml += "</ul>";
+  fetch(`${PICKLEBALL_URL}/${courtId}`,
+      options
+     )
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data); // response is a single object
+    let courtPic = data.fields["image"];
+    let courtName = data.fields["name"];
+    let courtDescription = data.fields["description"];
+    let courtAddress = data.fields["address"];
+    let courtHours = data.fields["hours"];
+    let courtURL = data.fields["url"];
+    let courtPhone = formatPhoneNumber(data.fields["phone"]);
+    let courtLanes = data.fields["lanes"];
+    let courtCost = data.fields["cost"];
+    let courtLeagues = data.fields["league"];
+    let hoursHtml = "";
+    if ("hours" in data.fields) {
+      hoursHtml += "<ul>";
+      let hours = data.fields["hours"].split("\n\n");
+      for (let i = 0; i < hours.length; i++) {
+        hoursHtml += `<li>${hours[i]}</li>`;
       }
-
-      let newHtml = `
-        <div class="row">
-          <a class="back-button btn w-auto col-3" href="./index.html">Back to Alleys List</a>
+      hoursHtml += "</ul>";
+    }
+    let newHtml = `
+      <div class="row">
+        <a class="back-button btn w-auto col-3" href="./index.html">Back to Courts List</a>
+      </div>
+      <div class="row">
+        <div class="col">
+          ${
+            courtPic
+              ? `<img class="details-image" src="${courtPic[0].url}" alt="Photo of ${courtName}">`
+              : ``
+          }
+          <h4>Official Website</h4>
+           <a href="${courtURL}" target="_blank">${courtName}</a>
+          <hr>
+          <h4>Contact</h4>
+          <p>${courtPhone}</p>
         </div>
-        <div class="row">
-          <div class="col">
-            ${
-              alleyPic
-                ? `<img class="details-image" src="${alleyPic[0].url}" alt="Photo of ${alleyName}">`
-                : ``
-            }
-            <h4>Official Website</h4>
-             <a href="${alleyURL}" target="_blank">${alleyName}</a>
+        <div class="col-lg-7">
+            <h2 id="details-title">${courtName} - ${courtLanes} Courts</h2>
             <hr>
-            <h4>Contact</h4>
-            <p>${alleyPhone}</p>
-          </div>
-          <div class="col-lg-7">
-              <h2 id="details-title">${alleyName} - ${alleyLanes} Lanes</h2>
-              <hr>
-              <h4>Description</h4>
-              <p>${alleyDescription}</p>
-              <hr>
-              <h4>Address</h4>
-              <p class="addresses">${alleyAddress}</p>
-              <hr>
-              <h4>Hours</h4>
-              <p>${hoursHtml}</p>
-              <hr>
-              <h4>Pricing</h4>
-              <p>${alleyCost}</p>
-              <hr>
-              <h4>League</h4>
-              <p>${alleyLeagues ? `${alleyName} has Leagues!` : `${alleyName} doesn't have Leagues.`}</p>
-          </div>
-        </row>
-      `;
-
-      alleyResultElement.innerHTML = newHtml;
-    });
+            <h4>Description</h4>
+            <p>${courtDescription}</p>
+            <hr>
+            <h4>Address</h4>
+            <p class="addresses">${courtAddress}</p>
+            <hr>
+            <h4>Hours</h4>
+            <p>${hoursHtml}</p>
+            <hr>
+            <h4>Pricing</h4>
+            <p>${courtCost}</p>
+            <hr>
+            <h4>League</h4>
+            <p>${courtLeagues ? `${courtName} has Leagues!` : `${courtName} doesn't have Leagues.`}</p>
+        </div>
+      </row>
+    `;
+    courtResultElement.innerHTML = newHtml;
+  });
 }
 
 // function searchFunction() {
@@ -174,7 +189,64 @@ function fetchSingleAlley(alleyId) {
 let idParams = window.location.search.split("?id=");
 if (idParams.length >= 2) {
   // has at least ["id?", "OUR ID"]
-  fetchSingleAlley(idParams[1]); // create detail view HTML w/ our id
+  fetchSingleCourt(idParams[1]); // create detail view HTML w/ our id
 } else {
-  fetchAlleys(); // no id given, fetch summaries
+  fetchCourts(); // no id given, fetch summaries
+}
+
+// --- Carousel for SF Pickleball Courts on Splash Page ---
+async function fetchSFCourtsForCarousel() {
+  const OUTDOOR_URL = "https://api.airtable.com/v0/appyPO0xrhPPEy72s/Outdoor";
+  const INDOOR_URL = "https://api.airtable.com/v0/appyPO0xrhPPEy72s/Indoor";
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer patSMmsMZV3ld7iSm.adf9b201bf5b4fad908e372c585816eb2521b5e7086b7a3c9418caaa099ad817`
+    },
+  };
+  // Fetch both Outdoor and Indoor courts
+  const [outdoorData, indoorData] = await Promise.all([
+    fetch(OUTDOOR_URL, options).then((response) => response.json()),
+    fetch(INDOOR_URL, options).then((response) => response.json()),
+  ]);
+  // Filter for SF courts (Zip starts with 941 or Address contains 'San Francisco')
+  const isSF = (fields) => {
+    return (fields["Zip"] && fields["Zip"].toString().startsWith("941")) ||
+      (fields["Address"] && fields["Address"].toLowerCase().includes("san francisco"));
+  };
+  let sfCourts = [...outdoorData.records, ...indoorData.records].filter(r => isSF(r.fields));
+  // Sort by 'Popularity' field descending (if present)
+  sfCourts = sfCourts.sort((a, b) => {
+    const popA = a.fields["Popularity"] || 0;
+    const popB = b.fields["Popularity"] || 0;
+    return popB - popA;
+  });
+  renderCarousel(sfCourts);
+}
+
+function renderCarousel(courts) {
+  const carouselInner = document.getElementById("carousel-inner");
+  if (!carouselInner) return;
+  let html = "";
+  for (let i = 0; i < courts.length && i < 5; i++) { // Show up to 5 courts
+    const fields = courts[i].fields;
+    const img = fields["Images"] ? fields["Images"][0].url : "";
+    const name = fields["Name"] || "Pickleball Court";
+    const address = fields["Address"] || "";
+    html += `
+      <div class="carousel-item${i === 0 ? ' active' : ''}">
+        <img src="${img}" class="d-block w-100" alt="${name}">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>${name}</h5>
+          <p>${address}</p>
+        </div>
+      </div>
+    `;
+  }
+  carouselInner.innerHTML = html;
+}
+
+// On splash page, run this to populate carousel
+if (window.location.pathname.endsWith("index.html") || window.location.pathname === "/") {
+  window.addEventListener("DOMContentLoaded", fetchSFCourtsForCarousel);
 }
